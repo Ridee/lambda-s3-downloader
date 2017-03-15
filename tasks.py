935a38753735177402d3f96120c2da8c7014a0fd
Javier Domingo Cansino <javierdo1@gmail.com>
2017-03-15 11:44:01 +0100

    Task to avoid zipping the git repo

--- tasks.py
+++ tasks.py
@@ -9,7 +9,7 @@ def prepare(ctx):
     """Generate environment installing dependencies"""
     ctx.run('pip install -t . -r lambda-requirements.txt')
     ctx.run('rm -f lambda.zip')
-    ctx.run('zip -r lambda.zip .')
+    ctx.run('zip -r lambda.zip *')
 
 
 if __name__ == '__main__':
